1340067a0a42e94972b1d120638fedce1657bd6d
isallbot <78047216+isallbot@users.noreply.github.com>
2021-02-05 05:27:45 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -519,13 +519,13 @@ const getRegisteredRandomId = () => {
 					for (let block of blocked) {
 						teks += `~> @${block.split('@')[0]}\n`
 					}
-					teks = `Total : ${blocked.length}`
+					teks += `Total : ${blocked.length}`
 					client.sendMessage(from, teks.trim(), extendedText, {quoted: mek, contextInfo: {"mentionedJid": blocked}})
 					break 
 				case 'banlist':
 				ben = '```List Banned``` :\n'
 					for (let banned of ban) {
-						ben += `~> @${banned.split('@')[0]}\n`
+						ben = `~> @${banned.split('@')[0]}\n`
 					}
 					ben = `Total : ${ban.length}`
 					client.sendMessage(from, ben.trim(), extendedText, {quoted: mek, contextInfo: {"mentionedJid": ban}})
